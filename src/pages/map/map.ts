44966f4b407279565e85fa95fd4a4221fea36264
ThiNotorious <theo.fombasso@gmail.com>
2018-10-16 11:34:26 +0200

    correction erreur

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -1,15 +1,6 @@
+import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-import {
-  GoogleMaps,
-  GoogleMap,
-  GoogleMapsEvent,
-  GoogleMapOptions,
-  CameraPosition,
-  MarkerOptions,
-  Marker,
-  Environment
-} from '@ionic-native/google-maps';
-import { Component } from "@angular/core/";
+
 /**
  * Generated class for the MapPage page.
  *
@@ -22,45 +13,12 @@ import { Component } from "@angular/core/";
   templateUrl: 'map.html',
 })
 export class MapPage {
-  map: GoogleMap;
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
+
   ionViewDidLoad() {
     console.log('ionViewDidLoad MapPage');
-    this.loadMap();
   }
-  loadMap() {
 
-    // This code is necessary for browser
-    Environment.setEnv({
-      'API_KEY_FOR_BROWSER_RELEASE': '(your api key for `https://`)',
-      'API_KEY_FOR_BROWSER_DEBUG': '(your api key for `http://`)'
-    });
-
-    let mapOptions: GoogleMapOptions = {
-      camera: {
-         target: {
-           lat: 43.0741904,
-           lng: -89.3809802
-         },
-         zoom: 18,
-         tilt: 30
-       }
-    };
-
-    this.map = GoogleMaps.create('map_canvas', mapOptions);
-
-    let marker: Marker = this.map.addMarkerSync({
-      title: 'Ionic',
-      icon: 'blue',
-      animation: 'DROP',
-      position: {
-        lat: 43.0741904,
-        lng: -89.3809802
-      }
-    });
-    marker.on(GoogleMapsEvent.MARKER_CLICK).subscribe(() => {
-      alert('clicked');
-    });
-  }
 }
